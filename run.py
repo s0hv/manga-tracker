fe8fcca81de082352ea39330cbf525ccb02030f3
s0hvaperuna <s0hvaperuna@gachimuchi.men>
2020-03-14 17:23:41 +0200

    Correct ordering in chapters

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 import json
 import os
-from datetime import datetime
-
-import pytz
+from datetime import datetime, timezone
 
 from src.scheduler import UpdateScheduler
 
@@ -10,4 +8,4 @@ with open(os.path.join('config', 'config.json'), encoding='utf-8') as f:
     config = json.load(f)
 
 scheduler = UpdateScheduler()
-print((scheduler.run_once()-datetime.utcnow().replace(tzinfo=pytz.utc)).total_seconds())
+print((scheduler.run_once()-datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(tz=timezone.utc)))
--- web/app.py
+++ web/app.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from contextlib import contextmanager
+from datetime import timezone
 
 import falcon
 from feedgen.feed import FeedGenerator
@@ -49,18 +50,19 @@ class Handler:
         sql = '''SELECT c.chapter_id, m.title as manga_title, c.title, c.chapter_number, c.release_date, c.chapter_identifier, s.service_name, s.chapter_url_format, s.url
                  FROM chapters c INNER JOIN manga m on c.manga_id = m.manga_id INNER JOIN services s on c.service_id = s.service_id WHERE c.release_date > NOW() - INTERVAL '1 hour'
                  UNION SELECT c.chapter_id, m.title as manga_title, c.title, c.chapter_number, c.release_date, c.chapter_identifier, s.service_name, s.chapter_url_format, s.url
-                 FROM chapters c INNER JOIN manga m on c.manga_id = m.manga_id INNER JOIN services s on c.service_id = s.service_id ORDER BY release_date, chapter_id
-                 LIMIT 20 - (SELECT COUNT(*) FROM chapters WHERE release_date > NOW() - INTERVAL '1 hour')'''
+                 FROM chapters c INNER JOIN manga m on c.manga_id = m.manga_id INNER JOIN services s on c.service_id = s.service_id ORDER BY release_date DESC, chapter_id
+                 LIMIT 40 - (SELECT COUNT(*) FROM chapters WHERE release_date > NOW() - INTERVAL '1 hour')'''
 
         with get_cursor() as cursor:
             cursor.execute(sql)
-            for row in cursor:
+            for row in reversed(cursor.fetchall()):
                 fe = fg.add_entry()
                 fe.title(row['title'])
                 fe.id(str(row['chapter_id']))
                 fe.link(href=row['chapter_url_format'].format(row['chapter_identifier']))
                 fe.source(title=row['service_name'], url=row['url'])
                 fe.description(f'{row["manga_title"]} - Chapter {row["chapter_number"]}')
+                fe.published(row['release_date'].astimezone(tz=timezone.utc))
 
         response.content_type = falcon.MEDIA_XML
         response.body = fg.rss_str(pretty=True)
