a94842160e3a07fba751d70bbb554011a265e3e2
s0hvaperuna <s0hvaperuna@gachimuchi.men>
2020-03-18 11:51:18 +0200

    update procfile and run.py

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn -b 0.0.0.0:$PORT run_web:api --worker-class=gevent
 clock: python run_clock.py
+worker python run.py
--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-import json
-import os
 from datetime import datetime, timezone
 
 from src.scheduler import UpdateScheduler
-
-with open(os.path.join('config', 'config.json'), encoding='utf-8') as f:
-    config = json.load(f)
 
 scheduler = UpdateScheduler()
 print((scheduler.run_once()-datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(tz=timezone.utc)))
