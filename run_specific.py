10b647ac158eb73e0e97e9f58d7dfe9a08780612
s0hvaperuna <s0hvaperuna@gachimuchi.men>
2020-05-14 21:59:09 +0300

    Remove testing code

--- run_specific.py
+++ run_specific.py
@@ -13,9 +13,4 @@ print(args)
 setup_logging.setup()
 
 scheduler = UpdateScheduler()
-#scheduler.force_run(args.service, args.manga)
-from src.scrapers import MangaPlus
-from src.utils.dbutils import DbUtil
-
-with scheduler.conn() as conn:
-    MangaPlus(conn, DbUtil(conn)).add_series(100037)
+scheduler.force_run(args.service, args.manga)
