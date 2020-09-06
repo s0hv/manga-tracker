eda95d5e99b231f3739a8e273698cb67b0827d35
s0hvaperuna <s0hvaperuna@gachimuchi.men>
2020-09-06 11:58:56 +0300

    Begin using sentry

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 from datetime import datetime, timezone
+import os
+
+import sentry_sdk
 
 import setup_logging
 from src.scheduler import UpdateScheduler
 
-logger = setup_logging.setup()
+sentry_sdk.init(
+    os.environ['SENTRY_URL'],
+    traces_sample_rate=1.0
+)
 
+logger = setup_logging.setup()
 scheduler = UpdateScheduler()
 logger.debug("Next update in %s", scheduler.run_once()-datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(tz=timezone.utc))
