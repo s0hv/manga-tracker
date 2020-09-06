b0dd7a10f3a979a1c243e8152bf2ac23edb2fff5
s0hvaperuna <s0hvaperuna@gachimuchi.men>
2020-09-06 12:00:31 +0300

    make sentry optional

--- run.py
+++ run.py
@@ -6,11 +6,15 @@ import sentry_sdk
 import setup_logging
 from src.scheduler import UpdateScheduler
 
-sentry_sdk.init(
-    os.environ['SENTRY_URL'],
-    traces_sample_rate=1.0
-)
-
 logger = setup_logging.setup()
+
+if 'SENTRY_URL' in os.environ:
+    sentry_sdk.init(
+        os.environ['SENTRY_URL'],
+        traces_sample_rate=1.0
+    )
+else:
+    logger.info('Skipping sentry initialization')
+
 scheduler = UpdateScheduler()
 logger.debug("Next update in %s", scheduler.run_once()-datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(tz=timezone.utc))
