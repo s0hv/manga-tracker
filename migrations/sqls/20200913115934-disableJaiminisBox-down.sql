7b3520490c77230096c5322b3da09de85f106ee3
s0hvaperuna <s0hvaperuna@gachimuchi.men>
2020-09-13 15:07:49 +0300

    Disable jaiminis box

---
+++ migrations/sqls/20200913115934-disableJaiminisBox-down.sql
@@ -0,0 +1,2 @@
+/* Replace with your SQL commands */
+UPDATE services SET disabled=FALSE WHERE service_id=3
---
+++ migrations/sqls/20200913115934-disableJaiminisBox-up.sql
@@ -0,0 +1,2 @@
+/* Replace with your SQL commands */
+UPDATE services SET disabled=TRUE WHERE service_id=3
