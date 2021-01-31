536b214c6c138012d4f6552e1d266b965b90a898
s0hv <s0hvaperuna@gachimuchi.men>
2021-01-31 21:08:12 +0200

    Simple manga view counter implementation

---
+++ web/utils/view-counter/manga-view-counter.js
@@ -0,0 +1,43 @@
+const db = require('../../db');
+
+const mangaView = (session, params) => {
+  if (!session) {
+    return;
+  }
+
+  if (!session.mangaViews) {
+    session.mangaViews = {};
+  }
+
+  const mangaId = params.manga_id;
+  if (!mangaId) {
+    return;
+  }
+
+  // Increment manga views for session
+  session.mangaViews[mangaId] = (session.mangaViews[mangaId] || 0) + 1;
+};
+
+/**
+ * Reads manga views from session and adds them to the database
+ */
+const onSessionExpire = (session) => {
+  if (!session || !session.mangaViews) {
+    return;
+  }
+
+  // List of args [$1, $2, $3, ...]
+  const args = [];
+  for (let i=1; i <= Object.keys(session.mangaViews).length; i++) {
+    args.push(`$${i}`);
+  }
+
+  // Increment views for each manga that was found by one
+  const sql = `UPDATE manga SET views=views+1 WHERE manga_id IN (${args.join(',')})`;
+  return db.query(sql, Object.keys(session.mangaViews));
+};
+
+module.exports = {
+  mangaView,
+  onSessionExpire,
+};
