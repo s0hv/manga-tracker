71408aedb0a058030cf5868bd9fbf66e3eb9ab1c
s0hv <s0hvaperuna@gachimuchi.men>
2021-03-01 19:58:34 +0200

    Fix undefined variable error

--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -111,7 +111,7 @@ def get_latest_chapters(rows: Iterable[Union[dict, DictRow]]) -> Dict[str, Tuple
     return chapter_data
 
 
-BaseChapter = TypeVar('BaseChapter', bound=Type['src.scrapers.base_scraper.BaseChapter'])
+BaseChapter = TypeVar('BaseChapter', bound=Type['src.scrapers.base_scraper.BaseChapter'])  # noqa: F821
 
 
 def group_by_manga(chapters: Iterable[BaseChapter]) -> Dict[str, List[BaseChapter]]:
