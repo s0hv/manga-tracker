89926b40aff7f28859a9d7f3664a96b079275d1d
s0hv <s0hvaperuna@gachimuchi.men>
2021-05-02 14:57:49 +0300

    Possible workaround for singleton db

--- web/db/index.js
+++ web/db/index.js
@@ -30,19 +30,34 @@ const pgp = require('pg-promise')({
   },
 });
 
-// Will cause warnings when in development mode
-const db = pgp({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: isTest ?
-    process.env.DB_NAME_TEST || process.env.DB_NAME :
-    process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  password: process.env.PGPASSWORD,
-  max: 5,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
+// https://stackoverflow.com/a/34427278/6046713
+const createSingletonDb = () => {
+  const s = Symbol.for('database');
+  let scope = global[s];
+  if (!global[s]) {
+    scope = pgp({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: isTest ?
+        process.env.DB_NAME_TEST || process.env.DB_NAME :
+        process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      password: process.env.PGPASSWORD,
+      max: 5,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,
+    });
+    global[s] = scope;
+  }
+  return scope;
+};
+
+/**
+ * Database object
+ * @type {pgPromise.IDatabase}
+ */
+const db = createSingletonDb();
+
 
 function query(sql, args) {
   return db.query(sql, args);
