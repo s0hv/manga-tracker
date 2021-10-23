7a2f8abe2f0a5490f5be9cb2919c51d0e2707e70
s0hv <s0hvaperuna@gachimuchi.men>
2021-10-23 14:15:59 +0300

    Pino fixes

--- web/utils/logging.js
+++ web/utils/logging.js
@@ -13,7 +13,7 @@ const pretty = process.env.PRETTY ?
 const logger = pino({
   transport: {
     target: pretty ? 'pino-pretty' : 'pino/file',
-    level,
+    level: level || 'warn',
     options: pretty ? {
       colorize: true,
       translateTime: 'SYS:HH:MM:ss.l',
