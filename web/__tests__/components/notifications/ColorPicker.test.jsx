d4e7a599bad72770b9571bb0afc77dfff6433082
s0hv <s0hvaperuna@gachimuchi.men>
2022-08-31 22:18:58 +0300

    update dependencies

--- web/__tests__/components/notifications/ColorPicker.test.jsx
+++ web/__tests__/components/notifications/ColorPicker.test.jsx
@@ -1,10 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { Form } from 'react-final-form';
 import ColorPicker from '../../../src/components/notifications/ColorPicker';
 
 const Root = ({ children }) => <Form onSubmit={jest.fn()} render={() => children} />;
+
+// https://github.com/omgovich/react-colorful/blob/c7e87161d71be9156b0c149d0e110fa24fd7037d/tests/components.test.js#L36
+// Mock `HTMLElement.getBoundingClientRect` to be able to read element sizes
+// See https://github.com/jsdom/jsdom/issues/135#issuecomment-68191941
+Object.defineProperties(HTMLElement.prototype, {
+  getBoundingClientRect: {
+    get: () => () => ({
+      left: 5,
+      top: 5,
+      x: 5,
+      y: 5,
+      width: 100,
+      height: 100,
+    }),
+  },
+});
+
+// Fix to pass `pageX` and `pageY`
+// See https://github.com/testing-library/react-testing-library/issues/268
+class FakeMouseEvent extends MouseEvent {
+  constructor(type, values = {}) {
+    super(type, { buttons: 1, bubbles: true, ...values });
+
+    Object.assign(this, {
+      pageX: values.pageX || 0,
+      pageY: values.pageY || 0,
+    });
+  }
+}
 
 describe('ColorPicker', () => {
   const testLabel = 'Color label for testing';
@@ -52,13 +81,12 @@ describe('ColorPicker', () => {
     expectColorWheelExist();
 
     const colorSlider = screen.getByRole('slider', { name: /color/i });
-    const { x, y } = colorSlider.getBoundingClientRect();
-    await user.pointer([
-      { keys: '[MouseLeft>]', target: colorSlider, coords: { pageX: x + 1, pageY: y + 1 }},
-      { keys: '[/MouseLeft]' },
-    ]);
 
-    expect(input.value).toMatch(/#\d{6}/i);
+    // user.pointer does not work here. input.value evaluates to #NaNNaNNaN
+    fireEvent(colorSlider, new FakeMouseEvent('mousedown', { pageX: 0, pageY: 0 }));
+    fireEvent(colorSlider, new FakeMouseEvent('mousemove', { pageX: 10, pageY: 10 }));
+
+    expect(input.value).toMatch(/#[a-f\d]{6}/i);
 
     expectColorWheelExist();
   });
