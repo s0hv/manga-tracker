1b28fb9cc14245ed6cd39a813f4168f5fde547fa
s0hv <s0hvaperuna@gachimuchi.men>
2022-11-03 19:32:32 +0200

    Disabled redoc temporarily

--- web/src/pages/swagger.jsx
+++ web/src/pages/swagger.jsx
@@ -1,8 +1,9 @@
-import { RedocStandalone } from 'redoc';
+// import { RedocStandalone } from 'redoc';
 import { getOpenapiSpecification } from '../../swagger';
 
 export default function Swagger({ spec }) {
-  return <RedocStandalone spec={spec} />;
+  return <div>Page temporarily disabled</div>;
+  // return <RedocStandalone spec={spec} />;
 }
 
 export async function getStaticProps() {
