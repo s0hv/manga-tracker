4a91a35ac0a48ac0a115a3d2e03df8adbc368dfa
s0hv <s0hvaperuna@gachimuchi.men>
2023-08-11 22:06:38 +0300

    Update dependencies

--- web/types/utility.ts
+++ web/types/utility.ts
@@ -1,4 +1,5 @@
 export type PartialExcept<T, K extends keyof T> = Pick<T, K> & Partial<Omit<T, K>>;
+export type RequiredExcept<T, K extends keyof T> = Required<Omit<T, K>> & Partial<Pick<T, K>>;
 
 /**
  * All parameters are of the base type except the ones given are partial
