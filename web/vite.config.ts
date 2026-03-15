8e688d7c5ac5b443830200ac6362226f5574a2a7
s0hv <s0hvaperuna@gachimuchi.men>
2026-03-15 16:36:13 +0200

    Update tanstack libraries to mitigate vulnerabilities

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,7 +2,6 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import istanbul from 'vite-plugin-istanbul';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // Base url must be defined in production
 const baseUrl = process.env.NODE_ENV === 'production'
